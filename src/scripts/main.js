c8ca10ccf90d1dffc45fad1e577d50219501b344
Zachetka <axi83@mail.ru>
2021-02-07 12:05:03 +0300

    add adaptive sections reviews

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,7 +11,27 @@ $(function(){
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true        
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]        
     });
 
     $('.menu__btn').on('click', function(){
